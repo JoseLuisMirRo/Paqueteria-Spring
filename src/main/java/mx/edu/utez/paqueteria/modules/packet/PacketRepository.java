abcfc1f1c877de9b2929dd66a74b45357b5688df
JoseLuisMirRo <169010668+JoseLuisMirRo@users.noreply.github.com>
2024-10-14 13:46:03 -0600

    [ADD] PacketRepository

--- src/main/java/mx/edu/utez/paqueteria/modules/packet/PacketRepository.java
+++ src/main/java/mx/edu/utez/paqueteria/modules/packet/PacketRepository.java
@@ -1,4 +1,32 @@
 package mx.edu.utez.paqueteria.modules.packet;
 
-public interface PacketRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface PacketRepository extends JpaRepository<Packet, Long> {
+    //Traer todos los pacquetes
+    List<Packet> findAll();
+
+    //Traer paquetes de un usuario
+    @Query(value = "SELECT * FROM packet WHERE id_user = :idUser", nativeQuery = true)
+    List<Packet> findAllByIdUser(@Param("idUser") long idUser);
+
+    //Traer paquete por id
+    Packet findById(long idPacket);
+
+    //Guardar/Actualizar paquetes
+    Packet save(Packet packet);
+
+    //Cambiar el estado del paquete
+    @Modifying
+    @Query(value = "UPDATE packet SET deliverded = :delivered, status = :status, WHERE id = :idPacket", nativeQuery = true)
+    Packet changeStatus(
+            @Param("deivered") boolean delivered,
+            @Param("status") PacketStatus status,
+            @Param("idPacket") long idPacket
+    );
 }
