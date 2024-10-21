d9fe9a2b6f28dc5239be61d7b690de8a1b4f54e4
JoseLuisMirRo <169010668+JoseLuisMirRo@users.noreply.github.com>
2024-10-21 13:27:52 -0600

    [FIX] ArticleRepository and PacketRepository

--- src/main/java/mx/edu/utez/paqueteria/modules/packet/PacketRepository.java
+++ src/main/java/mx/edu/utez/paqueteria/modules/packet/PacketRepository.java
@@ -4,9 +4,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface PacketRepository extends JpaRepository<Packet, Long> {
     //Traer todos los pacquetes
     List<Packet> findAll();
@@ -25,7 +27,7 @@ public interface PacketRepository extends JpaRepository<Packet, Long> {
     @Modifying
     @Query(value = "UPDATE packet SET deliverded = :delivered, status = :status, WHERE id = :idPacket", nativeQuery = true)
     Packet changeStatus(
-            @Param("deivered") boolean delivered,
+            @Param("delivered") boolean delivered,
             @Param("status") PacketStatus status,
             @Param("idPacket") long idPacket
     );
